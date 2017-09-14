2bdf3a626a9ee38c09795cdae79ceea808d2370a
dwightyy <pribeiro.rib@gmail.com>
2017-09-14 01:45:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,19 +8,23 @@ var opD = "d"
 qstList = [10,9,8,7,6,5,4,3,2,1]
 
 
-
-function getAns(op){ /* FUNCAO PARA COLOCAR TODAS AS SELECOES DO USUARIO EM UMA LISTA*/
+/* FUNCAO PARA COLOCAR TODAS AS SELECOES DO USUARIO EM UMA LISTA*/
+function getAns(op){ 
 	userAns.push(op)
 	return console.log(userAns);
 }
-function validade(lastOp){ 						/*FUNCAO PARA PASSAR O ULTIMO ELEMENTO DA LISTA DE SELECOES (RESPOSTA FINAL) DO USUARIO 
-														PARA UMA OUTRA LISTA, QUE IRA CONTER SOMENTE A RESPOSTA FINAL DAS QUESTOES.*/
+
+
+/*FUNCAO PARA PASSAR O ULTIMO ELEMENTO DA LISTA DE SELECOES (RESPOSTA FINAL) DO USUARIO
+PARA UMA OUTRA LISTA, QUE IRA CONTER SOMENTE A RESPOSTA FINAL DAS QUESTOES.*/
+function validade(lastOp){ 			  
 	lastAns.push(userAns[userAns.length - 1])
 	console.log(lastAns)
 	return lastAns	
 }
 
-function nextQ(qstList){ /*FUNCAO PARA SUMIR COM A ULTIMA PERGUNTA E COLOCAR A NOVA NA TELA, ATRAVES DA DIMINUICAO DE UMA LISTA*/
+/*FUNCAO PARA SUMIR COM A ULTIMA PERGUNTA E COLOCAR A NOVA NA TELA, ATRAVES DA DIMINUICAO DE UMA LISTA*/
+function nextQ(qstList){ 
 	if (qstList.length === 1){
 		document.getElementById("paginaPerguntas").style.display = "none"
 		document.getElementById('paginaFinal').style.display = "flex"
